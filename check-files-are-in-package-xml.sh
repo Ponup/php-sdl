195f2d9bba9dc165d37a465aa5c929b3943aa788
Santiago Lizardo <santiago.lizardo@gmail.com>
2018-09-22 12:20:21 +0100

    Prepare version 2.1.0-dev for release

--- check-files-are-in-package-xml.sh
+++ check-files-are-in-package-xml.sh
@@ -3,7 +3,7 @@ for i in *.c *.h examples/*.php tests/*.phpt
 do
 	j=$(basename $i)
 	if [ $j != config.h ]; then
-		grep -q "'$j'" package.xml || echo Missing $i
+		grep -q "['\"]$j['\"]" package.xml || echo Missing $i
 	fi
 done
 
